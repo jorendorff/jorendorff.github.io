5fc4a7ee257436043007302f6419613c0a7a5c23
Jason Orendorff <jason.orendorff@gmail.com>
2013-09-24 16:57:29 -0500

    Update to latest toy-calculator.

--- calc/calculator-backends.js
+++ calc/calculator-backends.js
@@ -287,95 +287,260 @@ assert.strictEqual(compileToJSFunction("x*x - 2*x + 1")(4), 9);
 
 // This one returns a JS function that operates on complex numbers.
 //
-// Here is a more advanced example of code generation.
+// This is a more advanced example of a compiler. It has a lowering phase
+// and a few simple optimizations.
+//
+// The input string `code` is a formula, for example, `"(z+1)/(z-1)"`,
+// that uses a complex variable `z`.
+//
+// This returns a JS function that takes two arguments, `z_re` and `z_im`,
+// the real and imaginary parts of a complex number *z*,
+// and returns an object `{re: some number, im: some number}`,
+// representing the complex result of computing the input formula,
+// (*z*+1)/(*z*-1). Here is the actual output in that case:
+//
+//     (function anonymous(z_re, z_im) {
+//     var t0 = (z_re+1);
+//     var t1 = (z_re-1);
+//     var t2 = (z_im*z_im);
+//     var t3 = ((t1*t1)+t2);
+//     return {re: (((t0*t1)+t2)/t3), im: (((z_im*t1)-(t0*z_im))/t3)};
+//     })
 //
 function compileToComplexFunction(code) {
-    var nextTmpId = 0;
+    // The first thing here is a lot of code about "values". This takes some
+    // explanation.
+    //
+    // `compileToComplexFunction` works in three steps.
+    //
+    // 1. It uses `parse` as its front end. We get an AST that represents
+    //    operations on complex numbers.
+    //
+    // 2. Then, it **lowers** the AST to an *intermediate representation* (IR)
+    //    that’s sort of halfway between the AST and JS code.
+    //
+    //    The IR here is an array of *values*, basic arithmetic operations on
+    //    plain old JS floating-point numbers. If we end up needing JS to
+    //    compute `(z_re + 1) * (z_re - 1), then each subexpression there ends
+    //    up being a value, represented by an object in the IR.
+    //
+    //    Once we have the IR, we can throw away the AST. The IR represents the
+    //    same formula, but in a different form. Lowering breaks down complex
+    //    arithmetic into sequences of JS numeric operations.  JS won't know
+    //    it's doing complex math.
+    //
+    // 3. Lastly, we convert the IR to JS code.
+    //
+    // **Why lowering?** We don't have to do it this way; we could define a
+    // class `Complex`, with methods `.add()`, `.sub()`, etc., and generate JS
+    // code that uses that.  Lowering leads to faster JS code: we'll generate
+    // code here that does not allocate any objects for intermediate results.
 
-    function genName() {
-        return "tmp" + nextTmpId++;
+    // #### About the IR
+    //
+    // Before we implement lower(), we need to define objects representing
+    // values. (We could just use strings of JS code, but it turns
+    // out to be really complicated, and it's hard to apply even basic
+    // optimizations to strings.)
+    //
+    // We call these instructions "values".  Each value represents a single JS
+    // expression that evaluates to a number.  A value is one of:
+    //
+    //   1. `z_re` or `z_im`, the real or imaginary part of the argument z; or
+    //   2. a numeric constant; or
+    //   3. an arithmetic operation, one of `+ - * /`, on two previously-
+    //      calculated values.
+    //
+    // And each value is represented by a plain old JSON object, as follows:
+    //
+    //   1. `{type: "arg", arg0: "z_re"` or `"z_im"}`
+    //   2. `{type: "number", arg0: (string picture of number)}`
+    //   3. `{type: (one of "+" "-" "*" "/"), arg0: (int), arg1: (int)}`
+    //
+    // All values are stored sequentially in the array `values`.  The two ints
+    // `v.arg0` and `v.arg1` in an arithmetic value are indexes into `values`.
+    //
+    // The main reason to store all values in a flat array, rather than a tree,
+    // is for "common subexpression elimination" (see valueToIndex).
+    //
+    var values = [
+        {type: "arg", arg0: "z_re", arg1: null},
+        {type: "arg", arg0: "z_im", arg1: null}
+    ];
+
+    // `values(n1, n2)` returns true if the two arguments `n1` and `n2` are
+    // equal values—that is, if it would be redundant to compute them both,
+    // because they are certain to have the same result.
+    //
+    // (This could be more sophisticated; it does not detect, for example, that
+    // a+1 is equal to 1+a.)
+    //
+    function valuesEqual(n1, n2) {
+        return n1.type === n2.type && n1.arg0 === n2.arg0 && n1.arg1 === n2.arg1;
     }
 
-    function emit(obj) {
+    // Return the index of `v` within `values`, adding it to `values` if needed.
+    function valueToIndex(v) {
+        // Common subexpression elimination. If we have already computed this
+        // exact value, instead of computing it again, just reuse the already-
+        // computed value.
+        for (var i = 0; i < values.length; i++) {
+            if (valuesEqual(values[i], v))
+                return i;
+        }
+
+        // We have not already computed this value, so add the value to
+        // the list.
+        values.push(v);
+        return values.length - 1;
+    }
+
+    function num(s) {
+        return valueToIndex({type: "number", arg0: s, arg1: null});
+    }
+
+    function op(op, a, b) {
+        return valueToIndex({type: op, arg0: a, arg1: b });
+    }
+
+    function isNumber(i) {
+        return values[i].type === "number";
+    }
+
+    function isZero(i) {
+        return isNumber(i) && values[i].arg0 === "0";
+    }
+
+    function isOne(i) {
+        return isNumber(i) && values[i].arg0 === "1";
+    }
+
+    function add(a, b) {
+        if (isZero(a))  // simplify (0+b) to b
+            return b;
+        if (isZero(b))  // simplify (a+0) to a
+            return a;
+        if (isNumber(a) && isNumber(b))  // constant-fold (1+2) to 3
+            return num(String(Number(values[a].arg0) + Number(values[b].arg0)));
+        return op("+", a, b);
+    }
+
+    function sub(a, b) {
+        if (isZero(b))  // simplify (a-0) to a
+            return a;
+        if (isNumber(a) && isNumber(b))  // constant-fold (3-2) to 1
+            return num(String(Number(values[a].arg0) - Number(values[b].arg0)));
+        return op("-", a, b);
+    }
+
+    function mul(a, b) {
+        if (isZero(a))  // simplify 0*b to 0
+            return a;
+        if (isZero(b))  // simplify a*0 to 0
+            return b;
+        if (isOne(a))  // simplify 1*b to b
+            return b;
+        if (isOne(b))  // simplify a*1 to a
+            return a;
+        if (isNumber(a) && isNumber(b))  // constant-fold (2*2) to 4
+            return num(String(Number(values[a].arg0) * Number(values[b].arg0)));
+        return op("*", a, b);
+    }
+
+    function div(a, b) {
+        if (isOne(b))  // simplify a/1 to a
+            return a;
+        if (isNumber(a) && isNumber(b) && !isZero(b))  // constant-fold 4/2 to 2
+            return num(String(Number(values[a].arg0) / Number(values[b].arg0)));
+        return op("/", a, b);
+    }
+
+    // Reduce obj, which represents an operation on complex numbers,
+    // to a pair of expressions on floating-point numbers.
+    function lower(obj) {
         switch (obj.type) {
         case "number":
-            return { setup: "", re: obj.value, im: "0" };
+            return {re: num(obj.value), im: num("0")};
 
         case "+": case "-":
-            var a = emit(obj.left), b = emit(obj.right);
+            var a = lower(obj.left), b = lower(obj.right);
+            var f = (obj.type === "+" ? add : sub);
             return {
-                setup: a.setup + b.setup,
-                re: a.re + " " + obj.type + " (" + b.re + ")",
-                im: a.im + " " + obj.type + " (" + b.im + ")"
+                re: f(a.re, b.re),
+                im: f(a.im, b.im)
             };
 
         case "*":
-            var a = emit(obj.left), b = emit(obj.right);
-
-            // This requires some setup.  First write some code to store the
-            // real and imaginary parts of a and b in temporary variables.
-            // We have to store them in temporary variables because the formula
-            // for complex multiplication uses each component twice, and we
-            // don’t want to compute them twice.
-            var atmp = genName(),
-                btmp = genName();
-            var setup = a.setup + b.setup +
-                ("var A_re = " + a.re + ", A_im = " + a.im + ";\n").replace(/A/g, atmp) +
-                ("var B_re = " + b.re + ", B_im = " + b.im + ";\n").replace(/B/g, btmp);
-
-            // Now return the setup, along with expressions for computing the
-            // real and imaginary parts of (a * b).
+            var a = lower(obj.left), b = lower(obj.right);
             return {
-                setup: setup,
-                re: "A_re * B_re - A_im * B_im".replace(/A/g, atmp).replace(/B/g, btmp),
-                im: "A_re * B_im + A_im * B_re".replace(/A/g, atmp).replace(/B/g, btmp)
+                re: sub(mul(a.re, b.re), mul(a.im, b.im)),
+                im: add(mul(a.re, b.im), mul(a.im, b.re))
             };
 
         case "/":
-            var a = emit(obj.left), b = emit(obj.right);
-
-            // Just as for multiplication, first write some code to store the real
-            // and imaginary parts of a and b in temporary variables.
-            var atmp = genName(),
-                btmp = genName(),
-                tmp = genName();
-            var setup = a.setup + b.setup +
-                ("var A_re = " + a.re + ", A_im = " + a.im + ";\n").replace(/A/g, atmp) +
-                ("var B_re = " + b.re + ", B_im = " + b.im + ";\n").replace(/B/g, btmp) +
-                ("var T = B_re * B_re + B_im * B_im;\n").replace(/T/g, tmp).replace(/B/g, btmp);
+            var a = lower(obj.left), b = lower(obj.right);
+            var t = add(mul(b.re, b.re), mul(b.im, b.im));
             return {
-                setup: setup,
-                re: "(A_re * B_re + A_im * B_im) / T".replace(/A/g, atmp).replace(/B/g, btmp).replace(/T/g, tmp),
-                im: "(A_im * B_re - A_re * B_im) / T".replace(/A/g, atmp).replace(/B/g, btmp).replace(/T/g, tmp)
+                re: div(add(mul(a.re, b.re), mul(a.im, b.im)), t),
+                im: div(sub(mul(a.im, b.re), mul(a.re, b.im)), t)
             };
 
         case "name":
             if (obj.id === "i")
-                return {setup: "", re: "0", im: "1"};
+                return {re: num("0"), im: num("1")};
             if (obj.id !== "z")
                 throw SyntaxError("undefined variable: " + obj.id);
-            return {
-                setup: "",
-                re: obj.id + "_re",
-                im: obj.id + "_im"
-            };
+            // A little subtle here: values[0] is z_re; values[1] is z_im.
+            return {re: 0, im: 1};
         }
     }
 
-    var result = emit(parse(code));
-    var code =
-        result.setup +
-        "return {re: " + result.re + ", im: " + result.im + "};\n";
+    function computeUseCounts(values) {
+        var binaryOps = {"+": 1, "-": 1, "*": 1, "/": 1};
+        var useCounts = [];
+        for (var i = 0; i < values.length; i++) {
+            useCounts[i] = 0;
+            var node = values[i];
+            if (node.type in binaryOps) {
+                useCounts[node.arg0]++;
+                useCounts[node.arg1]++;
+            }
+        }
+        return useCounts;
+    }
+
+    function ir_to_js(values, result) {
+        var useCounts = computeUseCounts(values);
+        var code = "";
+        var next_temp_id = 0;
+        var js = [];
+        for (var i = 0; i < values.length; i++) {
+            var node = values[i];
+            if (node.type === "number" || node.type === "arg")
+                js[i] = node.arg0;
+            else
+                js[i] = "(" + js[node.arg0] + node.type + js[node.arg1] + ")";
+
+            if (useCounts[i] > 1) {
+                var name = "t" + next_temp_id++;
+                code += "var " + name + " = " + js[i] + ";\n";
+                js[i] = name;
+            }
+        }
+        code += "return {re: " + js[result.re] + ", im: " + js[result.im] + "};\n";
+        return code;
+    }
+
+    var ast = parse(code);
+    var result = lower(ast);
+    var code = ir_to_js(values, result);
+    console.log(code);
     return Function("z_re, z_im", code);
 
     /*
       I had planned to have this generate asm.js code for extra speed, but it's
       so fast already that unless I can think of a more computationally
       intensive task, there is no need.
-         result.setup +
-         "var " + tmp + " = " + result.re + ";\n" +
-         "z_im = " + result.im + ";\n" +
-         "z_re = " + tmp + ";\n" +
     */
 }
 
